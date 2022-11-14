3de13226c15477d518bc25d3f4e313588c031ee7
OnagaHaruki <haloupgarou@gmail.com>
2022-11-14 16:36:28 +0900

    Initial Commit

---
+++ SwiftDiagram/SwiftDiagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDiagramApp.swift
+//  SwiftDiagram
+//
+//  Created by オナガ・ハルキ on 2022/11/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDiagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
