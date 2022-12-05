342127f193510776d5e29f71edba0d4fc7ac71b0
OnagaHaruki <haloupgarou@gmail.com>
2022-12-06 00:23:20 +0900

    syntaxTagのcaseに応じたswitch文を追加した。

--- SwiftDiagram/Model/SyntaxArrayParser.swift
+++ SwiftDiagram/Model/SyntaxArrayParser.swift
@@ -37,6 +37,17 @@ struct SyntaxArrayParser {
         var functionHolders = [String: FunctionHolder]()
         var extensionHolders = [String: ExtensionHolder]()
         
+        // 要素が宣言されてから、Holders辞書にインスタンスを格納するまでの間、一時的に情報を保持する
+        // 名前より前の情報はこのプロパティに格納する
+        // 名前を抽出した後、Holders辞書に格納する
+        var currentStructHolder = StructHolder()
+        var currentClassHolder = ClassHolder()
+        var currentEnumHolder = EnumHolder()
+        var currentProtocolHolder = ProtocolHolder()
+        var currentVariableHolder = VariableHolder()
+        var currentFunctionHolder = FunctionHolder()
+        var currentExtensionHolder = ExtensionHolder()
+        
         // holderStackArrayの要素
         struct HolderStackArrayElement {
             var holderType: HolderType
@@ -55,6 +66,284 @@ struct SyntaxArrayParser {
             // parsedElementArray[0]をSyntaxTag型にキャストする
             guard let syntaxTag = SyntaxTag(rawValue: parsedElementArray[0]) else {
                 fatalError("ERROR: Failed to convert \"\(parsedElementArray[0])\" to SyntaxTag")
+            }
+            
+            // syntaxTagのcaseに応じた処理をする
+            switch syntaxTag {
+            case .StartStructDeclSyntax:
+                currentStructHolder = StructHolder()
+            case .StructAccessLevel:
+                break
+            case .StructName:
+                break
+            case .EndStructDeclSyntax:
+                break
+            case .StartClassDeclSyntax:
+                currentClassHolder = ClassHolder()
+            case .ClassAccessLevel:
+                break
+            case .ClassName:
+                break
+            case .EndClassDeclSyntax:
+                break
+            case .StartEnumDeclSyntax:
+                currentEnumHolder = EnumHolder()
+            case .EnumAccessLevel:
+                break
+            case .EnumName:
+                break
+            case .RawvalueType:
+                break
+            case .Rawvalue:
+                break
+            case .CaseAssociatedValue:
+                break
+            case .StartEnumCaseElementSyntax:
+                break
+            case .EnumCase:
+                break
+            case .EndEnumCaseElementSyntax:
+                break
+            case .EndEnumDeclSyntax:
+                break
+            case .StartProtocolDeclSyntax:
+                currentProtocolHolder = ProtocolHolder()
+            case .ProtocolAccessLevel:
+                break
+            case .ProtocolName:
+                break
+            case .StartAssociatedtypeDeclSyntax:
+                break
+            case .AssociatedType:
+                break
+            case .ConformedProtocolOrInheritedClassByAssociatedType:
+                break
+            case .EndAssociatedtypeDeclSyntax:
+                break
+            case .EndProtocolDeclSyntax:
+                break
+            case .StartInheritedTypeListSyntax:
+                break
+            case .ConformedProtocolByStruct:
+                break
+            case .ConformedProtocolOrInheritedClassByClass:
+                break
+            case .ConformedProtocolByEnum:
+                break
+            case .ConformedProtocolByProtocol:
+                break
+            case .EndInheritedTypeListSyntax:
+                break
+            case .StartVariableDeclSyntax:
+                currentVariableHolder = VariableHolder()
+            case .VariableCustomAttribute:
+                break
+            case .IsStaticVariable:
+                break
+            case .LazyVariable:
+                break
+            case .VariableAccessLevel:
+                break
+            case .HaveLetKeyword:
+                break
+            case .VariableName:
+                break
+            case .VariableType:
+                break
+            case .StartArrayTypeSyntaxOfVariable:
+                break
+            case .ArrayTypeOfVariable:
+                break
+            case .EndArrayTypeSyntaxOfVariable:
+                break
+            case .StartDictionaryTypeSyntaxOfVariable:
+                break
+            case .DictionaryKeyTypeOfVariable:
+                break
+            case .DictionaryValueTypeOfVariable:
+                break
+            case .EndDictionaryTypeSyntaxOfVariable:
+                break
+            case .StartTupleTypeSyntaxOfVariable:
+                break
+            case .TupleTypeOfVariable:
+                break
+            case .EndTupleTypeSyntaxOfVariable:
+                break
+            case .ConformedProtocolByOpaqueResultTypeOfVariable:
+                break
+            case .IsOptionalType:
+                break
+            case .InitialValueOfVariable:
+                break
+            case .HaveWillSet:
+                break
+            case .HaveDidSet:
+                break
+            case .HaveGetter:
+                break
+            case .HaveSetter:
+                break
+            case .EndVariableDeclSyntax:
+                break
+            case .StartFunctionDeclSyntax:
+                currentFunctionHolder = FunctionHolder()
+            case .IsStaticFunction:
+                break
+            case .FunctionAccessLevel:
+                break
+            case .IsOverrideFunction:
+                break
+            case .IsMutatingFunction:
+                break
+            case .FunctionName:
+                break
+            case .StartFunctionParameterSyntax:
+                break
+            case .ExternalParameterName:
+                break
+            case .InternalParameterName:
+                break
+            case .HaveInoutKeyword:
+                break
+            case .IsVariadicParameter:
+                break
+            case .FunctionParameterType:
+                break
+            case .StartArrayTypeSyntaxOfFunctionParameter:
+                break
+            case .ArrayTypeOfFunctionParameter:
+                break
+            case .EndArrayTypeSyntaxOfFunctionParameter:
+                break
+            case .StartDictionaryTypeSyntaxOfFunctionParameter:
+                break
+            case .DictionaryKeyTypeOfFunctionParameter:
+                break
+            case .DictionaryValueTypeOfFunctionParameter:
+                break
+            case .EndDictionaryTypeSyntaxOfFunctionParameter:
+                break
+            case .StartTupleTypeSyntaxOfFunctionParameter:
+                break
+            case .TupleTypeOfFunctionParameter:
+                break
+            case .EndTupleTypeSyntaxOfFunctionParameter:
+                break
+            case .InitialValueOfParameter:
+                break
+            case .EndFunctionParameterSyntax:
+                break
+            case .StartFunctionReturnValueType:
+                break
+            case .FunctionReturnValueType:
+                break
+            case .StartArrayTypeSyntaxOfFunctionReturnValue:
+                break
+            case .ArrayTypeOfFunctionReturnValue:
+                break
+            case .EndArrayTypeSyntaxOfFunctionReturnValue:
+                break
+            case .StartDictionaryTypeSyntaxOfFunctionReturnValue:
+                break
+            case .DictionaryKeyTypeOfFunctionReturnValue:
+                break
+            case .DictionaryValueTypeOfFunctionReturnValue:
+                break
+            case .EndDictionaryTypeSyntaxOfFunctionReturnValue:
+                break
+            case .StartTupleTypeSyntaxOfFunctionReturnValue:
+                break
+            case .TupleTypeOfFunctionReturnValue:
+                break
+            case .EndTupleTypeSyntaxOfFunctionReturnValue:
+                break
+            case .ConformedProtocolByOpaqueResultTypeOfFunctionReturnValue:
+                break
+            case .EndFunctionReturnValueType:
+                break
+            case .EndFunctionDeclSyntax:
+                break
+            case .StartInitializerDeclSyntax:
+                break
+            case .HaveConvenienceKeyword:
+                break
+            case .IsFailableInitializer:
+                break
+            case .StartInitializerParameter:
+                break
+            case .InitializerParameterName:
+                break
+            case .InitializerParameterType:
+                break
+            case .StartArrayTypeSyntaxOfInitializer:
+                break
+            case .ArrayTypeOfInitializer:
+                break
+            case .EndArrayTypeSyntaxOfInitializer:
+                break
+            case .StartDictionaryTypeSyntaxOfInitializer:
+                break
+            case .DictionaryKeyTypeOfInitializer:
+                break
+            case .DictionaryValueTypeOfInitializer:
+                break
+            case .EndDictionaryTypeSyntaxOfInitializer:
+                break
+            case .StartTupleTypeSyntaxOfInitializer:
+                break
+            case .TupleTypeOfInitializer:
+                break
+            case .EndTupleTypeSyntaxOfInitializer:
+                break
+            case .EndInitializerParameter:
+                break
+            case .EndInitializerDeclSyntax:
+                break
+            case .StartExtensionDeclSyntax:
+                currentExtensionHolder = ExtensionHolder()
+            case .EndExtensionDeclSyntax:
+                break
+            case .ConformedProtocolByExtension:
+                break
+            case .StartGenericParameterSyntax:
+                break
+            case .ParameterTypeOfGenerics:
+                break
+            case .ConformedProtocolOrInheritedClassByGenerics:
+                break
+            case .EndGenericParameterSyntax:
+                break
+            case .StartTypealiasDecl:
+                break
+            case .TypealiasAssociatedTypeName:
+                break
+            case .TypealiasType:
+                break
+            case .StartArrayTypeSyntaxOfTypealias:
+                break
+            case .ArrayTypeOfTypealias:
+                break
+            case .EndArrayTypeSyntaxOfTypealias:
+                break
+            case .StartDictionaryTypeSyntaxOfTypealias:
+                break
+            case .DictionaryKeyTypeOfTypealias:
+                break
+            case .DictionaryValueTypeOfTypealias:
+                break
+            case .EndDictionaryTypeSyntaxOfTypealias:
+                break
+            case .StartTupleTypeSyntaxOfTypealias:
+                break
+            case .TupleTypeOfTypealias:
+                break
+            case .EndTupleTypeSyntaxOfTypealias:
+                break
+            case .EndTypealiasDecl:
+                break
+            case .Space:
+                break
             }
         }
         print("---------------------------------------")
