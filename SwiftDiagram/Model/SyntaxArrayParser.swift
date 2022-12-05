70107fd889bfa3e41170d7e297bc1def030b310c
OnagaHaruki <haloupgarou@gmail.com>
2022-12-05 14:15:28 +0900

    SyntaxArrayParser構造体を追加した。

---
+++ SwiftDiagram/Model/SyntaxArrayParser.swift
@@ -0,0 +1,13 @@
+//
+//  SyntaxArrayParser.swift
+//  SwiftDiagram
+//
+//  Created by オナガ・ハルキ on 2022/12/05.
+//
+
+import Foundation
+
+// TokenVisitorクラスが出力したresultArrayを解析し、各Holder構造体のインスタンスを生成する
+struct SyntaxArrayParser {
+    
+}
