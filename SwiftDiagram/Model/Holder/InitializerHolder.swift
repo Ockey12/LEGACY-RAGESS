e361b6e665a13aa6b937633716dbe8f9244613d5
OnagaHaruki <haloupgarou@gmail.com>
2022-12-06 01:07:01 +0900

    追加した。

---
+++ SwiftDiagram/Model/Holder/InitializerHolder.swift
@@ -0,0 +1,14 @@
+//
+//  InitializerHolder.swift
+//  SwiftDiagram
+//
+//  Created by オナガ・ハルキ on 2022/12/06.
+//
+
+import Foundation
+
+struct InitializerHolder: Holder {
+    var isConvenience = false
+    var isFailable = false
+    
+}
---
+++ SwiftDiagram/Model/Holder/InitializerParameterHolder.swift
@@ -0,0 +1,12 @@
+//
+//  InitializerParameterHolder.swift
+//  SwiftDiagram
+//
+//  Created by オナガ・ハルキ on 2022/12/06.
+//
+
+import Foundation
+
+struct InitializerParameterHolder {
+    var name: String = ""
+}
