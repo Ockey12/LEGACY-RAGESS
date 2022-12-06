5e553ce3f75dcfec19ba4d8e76c1ef4d04466262
OnagaHaruki <haloupgarou@gmail.com>
2022-12-06 23:33:40 +0900

    新しく作成した。

---
+++ SwiftDiagram/Model/Holder/VariableKind.swift
@@ -0,0 +1,16 @@
+//
+//  VariableKind.swift
+//  SwiftDiagram
+//
+//  Created by オナガ・ハルキ on 2022/12/06.
+//
+
+import Foundation
+
+// variableやfunctionParameterの型の種類
+enum VariableKind {
+    case literal // String, Intなど
+    case array
+    case dictionary
+    case tuple
+}
