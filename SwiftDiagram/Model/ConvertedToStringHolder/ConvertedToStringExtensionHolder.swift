84c964f6bc926ac4213c64fb845c173d3b168937
OnagaHaruki <haloupgarou@gmail.com>
2022-12-17 00:38:58 +0900

    Hashableプロトコルに準拠した。

--- SwiftDiagram/Model/ConvertedToStringHolder/ConvertedToStringExtensionHolder.swift
+++ SwiftDiagram/Model/ConvertedToStringHolder/ConvertedToStringExtensionHolder.swift
@@ -18,3 +18,33 @@ struct ConvertedToStringExtensionHolder {
     var nestingConvertedToStringClassHolders = [ConvertedToStringClassHolder]()
     var nestingConvertedToStringEnumHolders = [ConvertedToStringEnumHolder]()
 }
+
+extension ConvertedToStringExtensionHolder: Hashable {
+    static func == (lhs: ConvertedToStringExtensionHolder, rhs: ConvertedToStringExtensionHolder) -> Bool {
+        return (lhs.conformingProtocolNames == rhs.conformingProtocolNames) &&
+                (lhs.typealiases == rhs.typealiases) &&
+                (lhs.initializers == rhs.initializers) &&
+                (lhs.variables == rhs.variables) &&
+                (lhs.functions == rhs.functions) &&
+                (lhs.nestingConvertedToStringStructHolders == rhs.nestingConvertedToStringStructHolders) &&
+                (lhs.nestingConvertedToStringClassHolders == rhs.nestingConvertedToStringClassHolders) &&
+                (lhs.nestingConvertedToStringEnumHolders == rhs.nestingConvertedToStringEnumHolders)
+//        return lhs.conformingProtocolNames == rhs.conformingProtocolNames
+    }
+
+//    var hashValue: Int {
+//        return self.name.hashValue
+//    }
+
+    func hash(into hasher: inout Hasher) {
+        conformingProtocolNames.hash(into: &hasher)
+        typealiases.hash(into: &hasher)
+        initializers.hash(into: &hasher)
+        variables.hash(into: &hasher)
+        functions.hash(into: &hasher)
+        nestingConvertedToStringStructHolders.hash(into: &hasher)
+        nestingConvertedToStringClassHolders.hash(into: &hasher)
+        nestingConvertedToStringEnumHolders.hash(into: &hasher)
+//        ObjectIdentifier(self).hash(into: &hasher)
+    }
+}
