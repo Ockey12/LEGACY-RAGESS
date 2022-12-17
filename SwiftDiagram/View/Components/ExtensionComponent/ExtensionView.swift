d8ccf726104f217019250c16d1555ff26be40673
OnagaHaruki <haloupgarou@gmail.com>
2022-12-18 02:30:29 +0900

    Extensionコンポーネント内のNestコンポーネントを、ネストしたClassとEnumに対応させた。

--- SwiftDiagram/View/Components/ExtensionComponent/ExtensionView.swift
+++ SwiftDiagram/View/Components/ExtensionComponent/ExtensionView.swift
@@ -251,6 +251,93 @@ struct ExtensionView: View {
             height += nestBottomPadding
         } // for nestedStruct in nestedStructs
         
+        let nestedClasses = holder.nestingConvertedToStringClassHolders
+        for nestedClass in nestedClasses {
+            height += headerItemHeight + nestTopPaddingWithConnectionHeight
+            if 0 < nestedClass.generics.count {
+                height += connectionHeight
+                height += itemHeight*CGFloat(nestedClass.generics.count)
+                height += bottomPaddingForLastText
+            }
+            if let _ = nestedClass.superClassName {
+                height += connectionHeight
+                height += itemHeight
+                height += bottomPaddingForLastText
+            }
+            if 0 < nestedClass.conformingProtocolNames.count {
+                height += connectionHeight
+                height += itemHeight*CGFloat(nestedClass.conformingProtocolNames.count)
+                height += bottomPaddingForLastText
+            }
+            if 0 < nestedClass.typealiases.count {
+                height += connectionHeight
+                height += itemHeight*CGFloat(nestedClass.typealiases.count)
+                height += bottomPaddingForLastText
+            }
+            if 0 < nestedClass.initializers.count {
+                height += connectionHeight
+                height += itemHeight*CGFloat(nestedClass.initializers.count)
+                height += bottomPaddingForLastText
+            }
+            if 0 < nestedClass.variables.count {
+                height += connectionHeight
+                height += itemHeight*CGFloat(nestedClass.variables.count)
+                height += bottomPaddingForLastText
+            }
+            if 0 < nestedClass.functions.count {
+                height += connectionHeight
+                height += itemHeight*CGFloat(nestedClass.functions.count)
+                height += bottomPaddingForLastText
+            }
+            height += nestBottomPadding
+        } // for nestedClass in nestedClasses
+        
+        let nestedEnums = holder.nestingConvertedToStringEnumHolders
+        for nestedEnum in nestedEnums {
+            height += headerItemHeight + nestTopPaddingWithConnectionHeight
+            if 0 < nestedEnum.generics.count {
+                height += connectionHeight
+                height += itemHeight*CGFloat(nestedEnum.generics.count)
+                height += bottomPaddingForLastText
+            }
+            if let _ = nestedEnum.rawvalueType {
+                height += connectionHeight
+                height += itemHeight
+                height += bottomPaddingForLastText
+            }
+            if 0 < nestedEnum.conformingProtocolNames.count {
+                height += connectionHeight
+                height += itemHeight*CGFloat(nestedEnum.conformingProtocolNames.count)
+                height += bottomPaddingForLastText
+            }
+            if 0 < nestedEnum.typealiases.count {
+                height += connectionHeight
+                height += itemHeight*CGFloat(nestedEnum.typealiases.count)
+                height += bottomPaddingForLastText
+            }
+            if 0 < nestedEnum.initializers.count {
+                height += connectionHeight
+                height += itemHeight*CGFloat(nestedEnum.initializers.count)
+                height += bottomPaddingForLastText
+            }
+            if 0 < nestedEnum.cases.count {
+                height += connectionHeight
+                height += itemHeight*CGFloat(nestedEnum.cases.count)
+                height += bottomPaddingForLastText
+            }
+            if 0 < nestedEnum.variables.count {
+                height += connectionHeight
+                height += itemHeight*CGFloat(nestedEnum.variables.count)
+                height += bottomPaddingForLastText
+            }
+            if 0 < nestedEnum.functions.count {
+                height += connectionHeight
+                height += itemHeight*CGFloat(nestedEnum.functions.count)
+                height += bottomPaddingForLastText
+            }
+            height += nestBottomPadding
+        } // for nestedEnum in nestedEnums
+        
         return height
     } // func calculateFrameHeight() -> CGFloat
     
