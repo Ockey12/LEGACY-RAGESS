850794def9abf6ecca474d2ba6adafba5456e029
OnagaHaruki <haloupgarou@gmail.com>
2022-12-19 14:41:37 +0900

    コメントアウトしていたコードを削除した。

--- SwiftDiagram/ContentView.swift
+++ SwiftDiagram/ContentView.swift
@@ -169,68 +169,9 @@ struct ContentView: View {
 //functions: ["FunctionFunction", "FunctionFunctionFunction"]
 
     var body: some View {
-        MonitorView()
+//        MonitorView()
+        ScaleChangeableView()
             .frame(minWidth: 1000, maxWidth: .infinity, minHeight: 1000, maxHeight: .infinity)
-//        VStack {
-//            Spacer()
-////            IndexComponentFrameWithText(accessLevelIcon: AccessLevel.private.icon,
-////                                        headerComponentIndexType: .protocol)
-//            HStack {
-//                Spacer()
-//
-////                HeaderComponentFrame(bodyWidth: 800)
-////                    .stroke(lineWidth: 5)
-////                    .fill(.black)
-////                    .background(.pink)
-////                HeaderComponentFrameWithText(nameOfType: "AAAAAAAAAAAAAAAAAAAAA", bodyWidth: 1000)
-////                HeaderComponentView(accessLevelIcon: AccessLevel.internal.icon,
-////                                    indexType: .protocol,
-////                                    nameOfType: "SomeProtocol",
-////                                    bodyWidth: 800)
-////                DetailComponentFrame(bodyWidth: 1000,
-////                                     numberOfItems: 6)
-////                .stroke(lineWidth: 5)
-////                .fill(.black)
-////                DetailComponentView(componentType: .associatedType,
-////                                    strings: ["aaaaaaaaa",
-////                                             "bbbbbbbbbbbbbbbbbbb",
-////                                             "ccccc",
-////                                             "dddddddddddd"],
-////                                    bodyWidth: 1000)
-//
-//                ScrollView([.vertical, .horizontal]) {
-//                    StructView(holder: structHolder)
-//                        .frame(width: 3000, height: 8000)
-//                        .scaleEffect(0.3)
-//                }
-//                Spacer()
-//            } // HStack
-//            Spacer()
-//        } // VStack
-//        .frame(maxWidth: .infinity, maxHeight: .infinity)
-//        .background(.white)
-//        Text("Debug Now")
-//            .onAppear {
-//                let printer = TokenVisitorPrinter()
-//                printer.printResultArray()
-//            }
-//            .frame(minWidth: 1000, maxWidth: .infinity, minHeight: 1000, maxHeight: .infinity)
-//        ScrollView {
-//            SyntaxArrayParserView()
-//                .frame(minWidth: 1000, maxWidth: .infinity, minHeight: 1000, maxHeight: .infinity)
-//        }
-//        BuildFileMonitorView()
-//        GetFilesListOfDirectoryView()
-//        MonitorAndGetSourceFilesView()
-//        ScrollView {
-//            MonitorView()
-//            TokenVisitorView()
-//                .frame(minWidth: 1000, maxWidth: .infinity, minHeight: 1000, maxHeight: .infinity)
-//        }
-        
-//        IndexComponentFrame()
-//            .stroke(lineWidth: 3)
-//            .frame(width: 300, height: 90)
     }  // var body
 }
 
