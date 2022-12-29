7b59414c52951110253026eac2435eafd15ac762
OnagaHaruki <haloupgarou@gmail.com>
2022-12-29 17:01:59 +0900

    extensionを考慮して座標を取得できたが、issue#58の不具合がある。

--- SwiftDiagram/View/GetPoint/GetProtocolPointView.swift
+++ SwiftDiagram/View/GetPoint/GetProtocolPointView.swift
@@ -17,6 +17,7 @@ struct GetProtocolPointView: View {
     
     let textTrailPadding = ComponentSettingValues.textTrailPadding
     let arrowTerminalWidth = ComponentSettingValues.arrowTerminalWidth
+    let extensionOutsidePadding = ComponentSettingValues.extensionOutsidePadding
     
     var body: some View {
         ZStack {
@@ -25,10 +26,19 @@ struct GetProtocolPointView: View {
                     DispatchQueue.main.async {
                         arrowPoint.initialize()
                         for protocolHolder in monitor.getProtocol() {
+                            var currentPoint = arrowPoint.getCurrent()
+                            if 0 < protocolHolder.extensions.count {
+                                // extensionが宣言されているので、extensionコンポーネントの幅を考慮する
+                                currentPoint.x += extensionOutsidePadding - arrowTerminalWidth
+                            }
                             for (index, point) in arrowPoint.points.enumerated() {
                                 if point.affecter == protocolHolder.name {
                                     // TODO<extensionも考慮>
-                                    let currentPoint = arrowPoint.getCurrent()
+//                                    var currentPoint = arrowPoint.getCurrent()
+//                                    if 0 < protocolHolder.extensions.count {
+//                                        // extensionが宣言されているので、extensionコンポーネントの幅を考慮する
+//                                        currentPoint.x += extensionOutsidePadding
+//                                    }
                                     guard let width = maxWidthHolder.maxWidthDict[protocolHolder.name] else {
                                         continue
                                     }
@@ -41,7 +51,10 @@ struct GetProtocolPointView: View {
                             }
                             
                             if let maxWidth = maxWidthHolder.maxWidthDict[protocolHolder.name] {
-                                let newCurrentX = arrowPoint.getCurrent().x + maxWidth + textTrailPadding + arrowTerminalWidth*2 + 300 + 300 + 4
+                                var newCurrentX = currentPoint.x + maxWidth + textTrailPadding + arrowTerminalWidth*2 + 300 + 300 + 4
+                                if 0 < protocolHolder.extensions.count {
+                                    newCurrentX += extensionOutsidePadding - arrowTerminalWidth - 4
+                                }
                                 arrowPoint.setCurrentX(newCurrentX)
                             }
                         } // for protocolHolder in monitor.getProtocol()
